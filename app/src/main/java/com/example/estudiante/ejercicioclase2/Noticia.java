bbd8cace09a5875d9ee430a6f4cd702f394ce1d1
carolinac98 <carolinac98@hotmail.com>
2018-08-30 13:01:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/ejercicioclase2/Noticia.java
@@ -0,0 +1,40 @@
+package com.example.estudiante.ejercicioclase2;
+
+public class Noticia {
+
+    private String titulo;
+    private String descripcion;
+    //private string urlImage
+    private String fecha;
+
+
+    public Noticia(String titulo, String descripcion, String fecha) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
